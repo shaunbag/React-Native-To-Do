7c0e6aada43b26c75443f1c9e9c8b130eea69ff7
ShaunMcelhinney <shaun@vegascity.org>
2026-01-08 14:13:28 +0000

    Further styling

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import { View, Text, TextInput, Button, ScrollView } from 'react-native';
+import { View, Text, TextInput, Button, ScrollView, StyleSheet,ImageBackground } from 'react-native';
 import React from 'react';
 
 interface Todo {
@@ -35,17 +35,15 @@ export default function App() {
   };
 
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center', 
-      alignItems: 'center',  
-      backgroundColor: 'white'  
-    }}>
-      <Text>My To-do List</Text>
+    <View style={styles.container}>
+      <Text>My Todo List</Text>
       <TextInput 
         value={todoText} 
         onChangeText={setTodoText} 
         placeholder="Enter a new to-do"
+        style={{
+          padding: 10,
+        }}
       />
       <Button title="Add To-Do" onPress={addTodo}/>
       <Text>Tasks:</Text>
@@ -53,10 +51,10 @@ export default function App() {
         {
           todos.length > 0 ? (
             todos.map((todo) => {
-              return <View key={todo.id} style={{width: '80%'}}><Text onPress={() => toggleTodo(todo.id)}>{todo.completed ? '✓' : '○'} {todo.text}</Text></View>
+              return <View key={todo.id} style={styles.todoItem}><Text onPress={() => toggleTodo(todo.id)} style={todo.completed ? styles.completedTodo : {}}>{todo.completed ? '✓' : '○'} {todo.text}</Text></View>
             })
           ) : (
-            <Text style={{textAlign: 'center'}}>No tasks added yet.</Text>
+            <Text style={[styles.warning,{textAlign: 'center'}]}>Please Add Tasks!</Text>
           )
         }
       </ScrollView>
@@ -68,3 +66,32 @@ export default function App() {
     </View>
   );
 }
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#aee2e2ff',
+  },
+  warning: {
+    color: 'red',
+  },
+  todoItem: {
+    fontSize: 18,
+    marginVertical: 5,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#474747ff',
+    borderRadius: 5,
+    backgroundColor: '#e0e0e0',
+    shadowColor: '#000',
+    shadowOffset: { width: 1, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },  
+  completedTodo: {
+    textDecorationLine: 'line-through',
+    color: 'gray',
+  },
+})
